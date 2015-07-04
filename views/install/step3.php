79367777b2d7e2dc7e278f702fb9c52f1cb98216
Brian Parks <bparks@synapsesoftware.com>
2015-07-04 12:34:44 -0600

    Composer

--- views/install/step3.php
+++ views/install/step3.php
@@ -9,3 +9,7 @@ Here are some places you may want to go:</p>
 <li><a href="/admin">Admin Pages</a></li>
 <?php endif; ?>
 </ul>
+
+<p>You should also either remove the <code>installer</code> plugin (if this is a production environment)
+or make <code>config/application.php</code> read-only (to avoid trouncing the file in development).
+In general, <code>config/application.php</code> should never be writable by the web server user.</p> 
